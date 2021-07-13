1aeec58c95e4e78c67c86b8119790fa416d6c8ec
isalnz00 <73801144+isalnz00@users.noreply.github.com>
2021-07-13 22:21:19 +0200

    Trigger Warning added

--- warning.js
+++ warning.js
@@ -1,43 +1,52 @@
 import Button from "./button.js";
-import {fontA, fontG} from "./p5setup.js";
+import { fontA, fontG } from "./p5setup.js";
 
 export default class Warning extends Button {
-    constructor(x, y, width, height) {
-        super(x, y, width, height);
-        this.warning = false;
+  constructor(x, y, width, height) {
+    super(x, y, width, height);
+    this.warning = false;
+  }
+
+  display() {
+    stroke(0, 88, 106);
+    textSize(40);
+    textAlign(CENTER);
+    textFont(fontA);
+    text("*Trigger Warnung!*", 500, 100);
+    textSize(24);
+    textFont(fontG);
+    text(
+      "Die nachfolgende Simulation beinhaltet Themen wie psychische und körperliche Gewalt.",
+      500,
+      200
+    );
+    text(
+      "Bei manchen Menschen können diese Themen negative Reaktionen auslösen. ",
+      500,
+      240
+    );
+    text("Bitte sei achtsam, wenn das bei dir der Fall ist.", 500, 280);
+
+    fill(10, 50, 70, 180);
+    stroke(242, 145, 121);
+    strokeWeight(3);
+    rect(this.x, this.y, this.width, this.height, 15);
+
+    noStroke();
+    fill(210, 235, 244);
+    textSize(24);
+    text(
+      "Fortfahren",
+      this.x + 35,
+      this.y + 20,
+      this.width - 50,
+      this.height - 20
+    );
+  }
+
+  mouseClicked() {
+    if (this.hitTest(mouseX, mouseY)) {
+      this.warning = true;
     }
-
-
-    display() {
-        stroke(0, 88, 106);
-        textSize(40);
-        textAlign(CENTER);
-        textFont(fontA);
-        text("Trigger Warnung!", 500, 100);
-        textSize(24);
-        textFont(fontG);
-        text("[Insert Trigger Warning]", 500, 200);
-
-        fill(10, 50, 70, 180);
-        stroke(242, 145, 121);
-        strokeWeight(3);
-        rect(this.x, this.y, this.width, this.height, 15);
-
-        noStroke();
-        fill(210, 235, 244);
-        textSize(24);
-        text(
-         "Fortfahren",
-         this.x + 35,
-         this.y + 20,
-         this.width - 50,
-         this.height - 20
-        );
-    } 
-
-    mouseClicked() {
-        if (this.hitTest(mouseX, mouseY)) {
-          this.warning = true;
-        }
-      }
+  }
 }
